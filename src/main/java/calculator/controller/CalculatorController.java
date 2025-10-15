5d1d61a6e3dd5261cadcdeb8d46e94c4ce50bd0e
chanhk-im <cfasd1875@gmail.com>
2025-10-15 16:43:33 +0900

    feat: CalculatorController calculate 매서드 구현

--- src/main/java/calculator/controller/CalculatorController.java
+++ src/main/java/calculator/controller/CalculatorController.java
@@ -1,4 +1,15 @@
 package calculator.controller;
 
+import calculator.service.CalculatorService;
+
 public class CalculatorController {
+    private CalculatorService calculatorService;
+
+    public CalculatorController() {
+        this.calculatorService = new CalculatorService();
+    }
+
+    public int calculate(int[] numbers) {
+        return calculatorService.calculate(numbers);
+    }
 }
