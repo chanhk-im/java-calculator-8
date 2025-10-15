d4c9e4972d14e90ef9e83c9c504a757e21fb2214
chanhk-im <cfasd1875@gmail.com>
2025-10-15 21:07:58 +0900

    feat: Seperator seperate 매서드 구현

--- src/main/java/calculator/utils/Seperator.java
+++ src/main/java/calculator/utils/Seperator.java
@@ -1,4 +1,27 @@
 package calculator.utils;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Seperator {
+    public int[] seperate(List<Character> delimiters, String string) {
+        ArrayList<Integer> list = new ArrayList<>();
+
+        int currNumber = 0;
+        for (char c : string.toCharArray()) {
+            if (delimiters.contains(c)) {
+                list.add(currNumber);
+                currNumber = 0;
+            } else if (Character.isDigit(c)) {
+                currNumber = currNumber * 10 + Character.getNumericValue(c);
+            }
+        }
+
+        if (currNumber != 0) {
+            list.add(currNumber);
+        }
+
+        return list.stream().mapToInt(i -> i).toArray();
+    }
 }
