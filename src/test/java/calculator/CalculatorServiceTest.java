31248dec3133858566f41e1afe0f85d86f46a6a1
chanhk-im <cfasd1875@gmail.com>
2025-10-15 16:03:03 +0900

    test: CalculatorService RED test 작성

---
+++ src/test/java/calculator/CalculatorServiceTest.java
@@ -0,0 +1,30 @@
+package calculator;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class CalculatorServiceTest {
+    private CalculatorService calculatorService = new CalculatorService();
+
+    @Test
+    void Calculate_positive_number_is_successful() {
+        int[] arr = new int[]{
+                1, 2, 3
+        };
+
+        int result = calculatorService.calculate(arr);
+
+        Assertions.assertEquals(6, result);
+    }
+
+    @Test
+    void Calculate_negative_number_throws_exception() {
+        int[] arr = new int[]{
+                -1, 2, 3
+        };
+
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            calculatorService.calculate(arr);
+        });
+    }
+}
