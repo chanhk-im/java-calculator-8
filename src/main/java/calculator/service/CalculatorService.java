be5397d05e4201905e5025c385d48fde0382d23a
chanhk-im <cfasd1875@gmail.com>
2025-10-15 16:12:35 +0900

    feat: CalculatorService calculate 매서드 구현

--- src/main/java/calculator/service/CalculatorService.java
+++ src/main/java/calculator/service/CalculatorService.java
@@ -1,5 +1,16 @@
 package calculator.service;
 
 public class CalculatorService {
+    public int calculate(int[] numbers) {
+        int result = 0;
+        for (int number : numbers) {
+            if (number < 0) {
+                throw new IllegalArgumentException("양수를 입력해야 합니다.");
+            }
 
+            result += number;
+        }
+
+        return result;
+    }
 }
