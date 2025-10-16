848ba00aebf50dac33fbb0620d93175d533dbaf5
chanhk-im <cfasd1875@gmail.com>
2025-10-17 01:18:35 +0900

    feat: InputParser parseString 매서드 구현
    parseString 구현을 위해 정의한 매서드
    - hasDelimiter: 문자열이 커스텀 delimiter를 가지는 지 여부를 boolean으로 반환

--- src/main/java/calculator/utils/InputParser.java
+++ src/main/java/calculator/utils/InputParser.java
@@ -1,4 +1,38 @@
 package calculator.utils;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class InputParser {
+    private Seperator seperator;
+
+    public InputParser() {
+        seperator = new Seperator();
+    }
+
+    public boolean hasDelimiter(String input) {
+        if (input.charAt(0) == '/' && input.charAt(1) == '/' && input.indexOf('\n') != -1) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public int[] parseString(String input) {
+        if (hasDelimiter(input)) {
+            int delimiterEndIndex = input.indexOf("\n");
+            List<Character> delimiters = new ArrayList<>();
+
+            for (int i = 2; i < delimiterEndIndex; i++) {
+                delimiters.add(input.charAt(i));
+            }
+
+            String content = input.substring(delimiterEndIndex + 1);
+
+            return seperator.seperate(delimiters, content);
+        }
+
+        return seperator.seperate(Arrays.asList(',', ':'), input);
+    }
 }
