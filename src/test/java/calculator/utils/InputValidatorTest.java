4aacdfce4fce4bf07afcb91636e98c3ccbd11efd
chanhk-im <cfasd1875@gmail.com>
2025-10-17 02:13:51 +0900

    test: InputValidatorTest RED test 작성

--- src/test/java/calculator/utils/InputValidatorTest.java
+++ src/test/java/calculator/utils/InputValidatorTest.java
@@ -1,4 +1,44 @@
 package calculator.utils;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class InputValidatorTest {
+    private InputValidator validator = new InputValidator();
+
+    @Test
+    void Validate_successful() {
+        String input = "//;!@\n1;2!3";
+
+        boolean result = validator.checkValidate(input);
+
+        Assertions.assertTrue(result);
+    }
+
+    @Test
+    void Validate_no_newline_failure() {
+        String input = "//;!@1;2!3";
+
+        boolean result = validator.checkValidate(input);
+
+        Assertions.assertTrue(result);
+    }
+
+    @Test
+    void Validate_number_delimiter_failure() {
+        String input = "//;3!@\n1;2!3";
+
+        boolean result = validator.checkValidate(input);
+
+        Assertions.assertTrue(result);
+    }
+
+    @Test
+    void Validate_wrong_delimiter_failure() {
+        String input = "//;!@\n1#2!3";
+
+        boolean result = validator.checkValidate(input);
+
+        Assertions.assertTrue(result);
+    }
 }
