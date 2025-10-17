fd45a64e7ea5a93ca5d21cec70a58cb99dca2d83
chanhk-im <cfasd1875@gmail.com>
2025-10-17 16:20:18 +0900

    feat: InputValidator checkValidate 매서드 구현
    checkValidate 구현을 위해 정의한 매서드
    - hasDelimiter: 문자열이 커스텀 delimiter를 가지는 지 여부를 boolean으로 반환
    - hasNumberDelimiter: 문자열이 숫자 delimiter를 가지는 여부

--- src/main/java/calculator/utils/InputValidator.java
+++ src/main/java/calculator/utils/InputValidator.java
@@ -1,4 +1,57 @@
 package calculator.utils;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class InputValidator {
+    public boolean hasDelimiter(String input) {
+        if (input.charAt(0) == '/' && input.charAt(1) == '/' && input.indexOf('\n') != -1) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public boolean hasNumberDelimiter(String header) {
+        for (char ch : header.toCharArray()) {
+            if (Character.isDigit(ch)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public boolean checkValidate(String input) {
+        List<Character> delimiters = new ArrayList<Character>();
+        String content;
+
+        if (hasDelimiter(input)) {
+            int delimiterEndIndex = input.indexOf('\n');
+            String header = input.substring(2, delimiterEndIndex);
+
+            if (hasDelimiter(header) || hasNumberDelimiter(header)) {
+                return false;
+            }
+
+            for (char ch : header.toCharArray()) {
+                delimiters.add(ch);
+            }
+
+            content = input.substring(delimiterEndIndex + 1);
+        } else {
+            delimiters.add(',');
+            delimiters.add(':');
+
+            content = input;
+        }
+
+        for (char ch : content.toCharArray()) {
+            if (!Character.isDigit(ch) && !delimiters.contains(ch)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
 }
