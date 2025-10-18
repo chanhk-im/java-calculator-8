845cf5cacf1dd492d28ea18372abe17417f7d623
chanhk-im <cfasd1875@gmail.com>
2025-10-18 22:54:09 +0900

    feat: InputManager inputString 매서드 구현

--- src/main/java/calculator/controller/InputManager.java
+++ src/main/java/calculator/controller/InputManager.java
@@ -1,4 +1,25 @@
 package calculator.controller;
 
+import calculator.utils.InputValidator;
+import camp.nextstep.edu.missionutils.Console;
+
 public class InputManager {
+    private InputValidator inputValidator;
+
+    public InputManager() {
+        inputValidator = new InputValidator();
+    }
+
+    public String inputString() {
+        String input = Console.readLine();
+        if (input.matches("^//\\S+")) {
+            String content = Console.readLine();
+            input += "\n" + content;
+        }
+        if (!inputValidator.checkValidate(input)) {
+            throw new IllegalArgumentException("올바르지 않은 입력입니다.");
+        }
+
+        return input;
+    }
 }
