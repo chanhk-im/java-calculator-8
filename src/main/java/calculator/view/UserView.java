83b8aa742cc5f6840983328df447a780fd2c80d3
chanhk-im <cfasd1875@gmail.com>
2025-10-18 23:28:17 +0900

    refactor: UserView String 상수화

--- src/main/java/calculator/view/UserView.java
+++ src/main/java/calculator/view/UserView.java
@@ -2,10 +2,10 @@ package calculator.view;
 
 public class UserView {
     public void showInputMessage() {
-        System.out.println("합계를 구할 숫자를 입력하세요.");
+        System.out.println(UserViewConstant.INPUT_MESSAGE);
     }
 
     public void showResult(int result) {
-        System.out.println("결과 : " + result);
+        System.out.println(UserViewConstant.RESULT_MESSAGE + result);
     }
 }
---
+++ src/main/java/calculator/view/UserViewConstant.java
@@ -0,0 +1,9 @@
+package calculator.view;
+
+public class UserViewConstant {
+    private UserViewConstant() {
+    }
+
+    public static final String INPUT_MESSAGE = "합계를 구할 숫자를 입력하세요.";
+    public static final String RESULT_MESSAGE = "결과 : ";
+}
