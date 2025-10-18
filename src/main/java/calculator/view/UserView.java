cb6939e63e56db9dae7ab7d8389cf73b8dc0d6c7
chanhk-im <cfasd1875@gmail.com>
2025-10-18 23:20:29 +0900

    feat: UserView showInputMessage, showResult 매서드 구현

--- src/main/java/calculator/view/UserView.java
+++ src/main/java/calculator/view/UserView.java
@@ -1,4 +1,11 @@
 package calculator.view;
 
 public class UserView {
+    public void showInputMessage() {
+        System.out.println("합계를 구할 숫자를 입력하세요.");
+    }
+
+    public void showResult(int result) {
+        System.out.println("결과 : " + result);
+    }
 }
