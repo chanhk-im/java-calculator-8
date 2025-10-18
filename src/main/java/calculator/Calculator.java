e8d763cb69eac52d6df61f3d566421ba1be96f28
chanhk-im <cfasd1875@gmail.com>
2025-10-18 23:38:35 +0900

    feat: Calculator 구현

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,4 +1,31 @@
 package calculator;
 
+import calculator.controller.CalculatorController;
+import calculator.controller.InputManager;
+import calculator.utils.InputParser;
+import calculator.view.UserView;
+
 public class Calculator {
+    private InputManager inputManager;
+    private InputParser inputParser;
+    private CalculatorController controller;
+    private UserView view;
+
+    public Calculator() {
+        inputManager = new InputManager();
+        inputParser = new InputParser();
+        controller = new CalculatorController();
+        view = new UserView();
+    }
+
+    public void start() {
+        view.showInputMessage();
+        String input = inputManager.inputString();
+
+        int[] numbers = inputParser.parseString(input);
+
+        int result = controller.calculate(numbers);
+
+        view.showResult(result);
+    }
 }
