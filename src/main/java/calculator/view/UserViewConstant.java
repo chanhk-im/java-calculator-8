e05acf77d7421920437dfa8f37b2233a7526d099
chanhk-im <cfasd1875@gmail.com>
2025-10-19 18:36:00 +0900

    fix: input message 변경

--- src/main/java/calculator/view/UserViewConstant.java
+++ src/main/java/calculator/view/UserViewConstant.java
@@ -4,6 +4,6 @@ public class UserViewConstant {
     private UserViewConstant() {
     }
 
-    public static final String INPUT_MESSAGE = "합계를 구할 숫자를 입력하세요.";
+    public static final String INPUT_MESSAGE = "덧셈할 문자열을 입력해 주세요.";
     public static final String RESULT_MESSAGE = "결과 : ";
 }
